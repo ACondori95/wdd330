69aacc49d8788eb5aa50b2944fa2788b8e1d4c22
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-05-23 21:59:07 -0300

    Add files via upload

---
+++ wdd_330/script/date-modified.js
@@ -0,0 +1,3 @@
+const lastModDate = new Date(document.lastModified);
+let strMD = 'Last Updated: ' + lastModDate.getMonth() + '/' + lastModDate.getDate() + '/' + lastModDate.getFullYear() + ' --- ' + lastModDate.getHours() + ':' + lastModDate.getMinutes() + ':' + lastModDate.getSeconds();
+document.getElementById('date-modified').textContent = strMD;
