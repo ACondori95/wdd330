fc615e4655fa9abb36c9f32fc474c17cdc2575a3
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-05-23 22:02:16 -0300

    update

--- wdd_330/script/date-modified.js
+++
@@ -1,3 +0,0 @@
-const lastModDate = new Date(document.lastModified);
-let strMD = 'Last Updated: ' + lastModDate.getMonth() + '/' + lastModDate.getDate() + '/' + lastModDate.getFullYear() + ' --- ' + lastModDate.getHours() + ':' + lastModDate.getMinutes() + ':' + lastModDate.getSeconds();
-document.getElementById('date-modified').textContent = strMD;
